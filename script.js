7f2c156017490975a1877e6914a1ec32465fcef8
Célia Tosic <celiatosic@gmail.com>
2021-11-02 20:38:59 +0100

    Modification de l'emplacement des icônes erreurs en %. Le visuel n'était pas le même entre Firefox et Chrome

--- script.js
+++ script.js
@@ -16,36 +16,36 @@ let errorEmail = document.getElementById("error_email");
 submitButton.addEventListener('click', validation);
 
 function validation(e) {
+
     if (firstName.validity.valueMissing) {
         e.preventDefault();
         errorFirstName.innerHTML = "First Name cannot be empty";
         firstName.style.borderColor = "hsl(0, 100%, 74%)";
         firstName.style.background = " url(images/icon-error.svg) no-repeat right";
-        firstName.style.backgroundPositionX = "right 20px";
-        password.style.backgroundPositionY = "10px";
+        firstName.style.backgroundPositionX = "95%";
     }
+
     if (lastName.validity.valueMissing) {
         e.preventDefault();
         errorLastName.innerHTML = "Last Name cannot be empty";
         lastName.style.borderColor = "hsl(0, 100%, 74%)";
         lastName.style.background = " url(images/icon-error.svg) no-repeat right";
-        lastName.style.backgroundPositionX = "right 20px";
-        password.style.backgroundPositionY = "10px";
+        lastName.style.backgroundPositionX = "95%";
     }
+
     if (password.validity.valueMissing) {
         e.preventDefault();
         errorPassword.innerHTML = "Password cannot be empty";
         password.style.borderColor = "hsl(0, 100%, 74%)";
         password.style.background = " url(images/icon-error.svg) no-repeat right ";
-        password.style.backgroundPositionX = "right 20px";
-        password.style.backgroundPositionY = "10px";
+        password.style.backgroundPositionX = "95%";
     }
+
     if (!/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/.test(email.value)) {
         errorEmail.innerHTML = "Looks like this is not an email";
         email.style.color = "hsl(0, 100%, 74%)";
         email.style.borderColor = "hsl(0, 100%, 74%)";
         email.style.background = " url(images/icon-error.svg) no-repeat right";
-        email.style.backgroundPositionX = "right 20px";
-        password.style.backgroundPositionY = "10px";
+        email.style.backgroundPositionX = "95%";
     }
 }
